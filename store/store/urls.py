c87554b37094cadc6ff559b9fac9dbf4f812e08e
kittisitthichai <kittisitthichai30084@gmail.com>
2019-07-24 10:19:42 +0700

    init projects

---
+++ store/store/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings  #include setting form file setting 
+from django.conf.urls.static import static #call static in setting
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
